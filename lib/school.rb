66ee196979656d03c6d815833e5ae4cb25fa8d18
Justin <jdanielmcarthur@msn.com>
2021-07-02 08:43:48 -0700

    commit tree ef16f27dd47e1e47b3df7558a79b21ca570c2522

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,40 @@
-# code here!
+require "pry"
+
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name = "Test School", roster = {})
+        @name = name
+        @roster = roster
+    end
+
+    def add_student(name, grade)
+        if @roster[grade]
+            @roster[grade] << name
+        else 
+            @roster[grade] =[]
+            @roster[grade] << name
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        sorted_hash = {}
+        @roster.sort.each {|grade, name_array|}
+            sorted_hash[grade] = (name_array.sort)
+        end 
+        sorted_hash
+    end
+
+end
+
+school = School.new
+school.add_student("Daniel", 9)
+school.add_student("Zaniel", 9)
+school.add_student("Baniel", 10)
+school.add_student("Maniel", 11)
+
+binding.pry
